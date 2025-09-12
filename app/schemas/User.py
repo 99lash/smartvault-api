91a9c1e7aa60ac0448ff5c1645ec761791f52598
Asher <asherjohn48@gmail.com>
2025-09-12 15:13:26 +0800

    feature: added UserRead for HTTP response schemas.

--- app/schemas/User.py
+++ app/schemas/User.py
@@ -1,6 +1,7 @@
 from sqlmodel import SQLModel
 from pydantic import EmailStr
 from app.models.User import UserRole
+from datetime import datetime
 
 # ----------------------------
 # User HTTP Request Schemas
@@ -20,6 +21,20 @@ class UpdateUserRole(SQLModel, table=False):
 
 
 # ----------------------------
-# User HTTP Request Schemas
+# User HTTP Response Schemas
 # ----------------------------
 # Tiyaka na 'to kasi yung response naman ay nagawa na sa `app/models/User.py`
+class UserRead(SQLModel, table=False):
+  id: int
+  username: str
+  email: EmailStr
+  password_hash: str
+  # ** Pwedeng i-modify rito yung user properties ng HTTP response.
+  # ** Example: i-comment mo or tanggalin mo yung `password_hash` property.    
+  role: UserRole
+  created_at: datetime
+  updated_at: datetime | None
+  deleted_at: datetime | None
+  
+  class Config: 
+    from_attributes = True
