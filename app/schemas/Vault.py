a0d8392d9f42856a8c44465d1f60a1664fcfd277
Asher <asherjohn48@gmail.com>
2025-09-13 20:37:51 +0800

    feat: added vault HTTP request / response schemas.

---
+++ app/schemas/Vault.py
@@ -0,0 +1,30 @@
+from sqlmodel import SQLModel
+from pydantic import EmailStr
+from datetime import datetime
+from typing import Optional
+from app.models.Vault import VaultStatus
+
+# ----------------------------
+# Vault HTTP Request Schemas
+# ----------------------------
+class VaultCreate(SQLModel, table=False):
+  name: str
+  location: Optional[str] = None
+
+class UpdateVaultStatus(SQLModel, table=False):
+  status: VaultStatus
+
+# ----------------------------
+# Vault HTTP Response Schemas
+# ----------------------------
+class VaultRead(SQLModel, table=False):
+  id: int
+  name: str
+  location: Optional[str] = None
+  status: VaultStatus
+  created_at: datetime
+  updated_at: Optional[datetime] = None
+  deleted_at: Optional[datetime] = None
+
+  class Config:
+    from_attributes = True
