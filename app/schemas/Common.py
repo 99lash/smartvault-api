fe991151cc091c3d60669a02b0935b9db7048eb9
Asher <asherjohn48@gmail.com>
2025-09-13 14:33:41 +0800

    feat: added `Response` class for HTTP response.

---
+++ app/schemas/Common.py
@@ -0,0 +1,9 @@
+from typing import Generic, TypeVar, Optional
+from pydantic import BaseModel
+
+T = TypeVar("T")
+
+class Response(BaseModel, Generic[T]):
+    success: bool = True
+    data: Optional[T] = None
+    message: Optional[str] = None
