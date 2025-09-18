a78e2ca37b10e80115374bfaad291fd7fff04ac9
Asher <asherjohn48@gmail.com>
2025-09-18 15:35:00 +0800

    refactor: renamed the `user_vault.py` schema file to follow PEP 8 module file naming convention instead of class naming convention.

--- app/routes/uservault_routes.py
+++ app/routes/uservault_routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.core.database import get_db
 from app.schemas.Response import Response
-from app.schemas.UserVault import UserVaultCreate, UserVaultRead, UserVaultBulkCreate
+from app.schemas.user_vault import UserVaultCreate, UserVaultRead, UserVaultBulkCreate
 from app.schemas.vault import VaultRead
 from app.schemas.user import UserRead
 from app.services.UserVaultService import UserVaultService
