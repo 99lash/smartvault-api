bdd8e74b4201ea5b31e0cefd1b99afdd64366965
Asher <asherjohn48@gmail.com>
2025-10-06 22:14:57 +0800

    refactor: added OAuth2Scheme to get current user

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,24 +1,40 @@
 from fastapi import Depends, Header, HTTPException
+from fastapi.security import OAuth2PasswordBearer;
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.services.users.UserService import UserService
 from app.models.User import User
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/users/login');
 
+
+# @XpnsiveSharks try mo 'tong `get_current_user` na hindi naka comment. Mas safe 'to kaysa sa naka comment na `get_current_user sa ibaba` Kasi hindi expose. 
 def get_current_user(
-    authorization: str = Header(..., description="Bearer access token"),
+    token = Depends(oauth2_scheme),
     db: Session = Depends(get_db)
 ) -> User:
     """
     Dependency to extract and validate the current user from the Authorization header.
     Raises 401 if no valid Bearer token is provided.
     """
-    if not authorization or not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Not authenticated")
-
-    token = authorization.split(" ")[1]
     user_service = UserService(db)
     return user_service.validate_token(token)
+
+
+# def get_current_user(
+#     authorization: str = Header(..., description="Bearer access token"),
+#     db: Session = Depends(get_db)
+# ) -> User:
+#     """
+#     Dependency to extract and validate the current user from the Authorization header.
+#     Raises 401 if no valid Bearer token is provided.
+#     """
+#     if not authorization or not authorization.startswith("Bearer "):
+#         raise HTTPException(status_code=401, detail="Not authenticated")
+
+#     token = authorization.split(" ")[1]
+#     user_service = UserService(db)
+#     return user_service.validate_token(token)
 
 
 def get_current_admin(
